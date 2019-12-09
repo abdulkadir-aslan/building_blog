c8372c8f38e80c1ce3e0f89f971c6b5cc01e77a4
abdulkadiraslan <abdulkadiraslan@gmail.com>
2019-12-10 02:49:23 +0300

    Post model add

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+STATUS = [
+    ('draft','Taslak'),
+    ('published','Yayinlandi'),
+    ('deleted','silindi'),
+]
+DEFAULT_STATUS ="draft"
+
+class Post(models.Model)
+    title = models.CharField(max_length=150,default=False)
+    slug = models.CharField(max_length=150,unique=True)
+    content = models.TextField()
+    cover_image = models.ImageField(upload_to='post',blank=True)
+    status = models.CharField(max_length=10,STATUS,default=DEFAULT_STATUS)
+    is_home = models.BooleanField(default=False) 
+    creat_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+        
+
