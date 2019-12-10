304a27aa588163a3fa176a03ed13f50b5cd798b4
abdulkadiraslan <abdulkadiraslan@gmail.com>
2019-12-10 03:16:20 +0300

    admin,setting add

--- blog/models.py
+++ blog/models.py
@@ -7,16 +7,16 @@ STATUS = [
 ]
 DEFAULT_STATUS ="draft"
 
-class Post(models.Model)
-    title = models.CharField(max_length=150,default=False)
+class Post(models.Model):
+    title = models.CharField(max_length=150,blank=True)
     slug = models.CharField(max_length=150,unique=True)
     content = models.TextField()
     cover_image = models.ImageField(upload_to='post',blank=True)
-    status = models.CharField(max_length=10,STATUS,default=DEFAULT_STATUS)
+    status = models.CharField(max_length=10,choices=STATUS,default=DEFAULT_STATUS)
     is_home = models.BooleanField(default=False) 
     creat_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
-        
+
 
