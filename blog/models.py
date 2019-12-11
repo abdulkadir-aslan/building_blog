8606ee3fe1ae5368cb993f3dfbf8c2edf9292cf2
abdulkadiraslan <abdulkadiraslan@gmail.com>
2019-12-11 12:35:05 +0300

    Tags added

--- blog/models.py
+++ blog/models.py
@@ -18,11 +18,21 @@ class Category(models.Model):
         return self.title
     
 
+class Tag(models.Model):
+    title = models.CharField(max_length=100,blank=True)
+    slug = models.SlugField(max_length=100,blank=True,unique=True)
+    status = models.CharField(max_length=10,choices=STATUS,default=DEFAULT_STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at  =models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
-    categories = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    tags = models.ManyToManyField(Tag)
     title = models.CharField(max_length=150,blank=True)
-    slug = models.SlugField(max_length=150,unique=True)
+    slug = models.SlugField(max_length=150,unique=True,blank=True)
     content = models.TextField()
     cover_image = models.ImageField(upload_to='post',blank=True)
     status = models.CharField(max_length=10,choices=STATUS,default=DEFAULT_STATUS)
