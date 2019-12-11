209941c5b463471929c08310e0c9bbd31c917156
abdulkadiraslan <abdulkadiraslan@gmail.com>
2019-12-11 12:22:24 +0300

    Category added

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,22 @@ STATUS = [
 ]
 DEFAULT_STATUS ="draft"
 
-class Post(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=150,blank=True)
-    slug = models.CharField(max_length=150,unique=True)
+    slug = models.SlugField(max_length=150,unique=True)
+    content = models.TextField()
+    status= models.CharField(max_length=10,choices=STATUS,default=DEFAULT_STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at =models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+    
+
+
+class Post(models.Model):
+    categories = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=150,blank=True)
+    slug = models.SlugField(max_length=150,unique=True)
     content = models.TextField()
     cover_image = models.ImageField(upload_to='post',blank=True)
     status = models.CharField(max_length=10,choices=STATUS,default=DEFAULT_STATUS)
