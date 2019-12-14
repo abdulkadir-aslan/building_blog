355fda69cdc7685cf4e714c4509555038c775261
abdulkadiraslan <abdulkadiraslan@gmail.com>
2019-12-14 18:17:42 +0300

    logout added

--- py_blog/urls.py
+++ py_blog/urls.py
@@ -9,6 +9,7 @@ from user_blog import views as blog_views_user
 urlpatterns = [
     path('',blog_views.index,name='home'),
     path('login/',blog_views_user.user_login,name='login'),
+    path('logout/',blog_views_user.user_logout,name='logout'),
     path('category/<slug:tag_slug>/', blog_views.index, name='cat'),
     path('category/<slug:tag_slug>/<slug:post_slug>/',blog_views.tag_post,name='post'),
     path('admin/', admin.site.urls),
--- user_blog/views.py
+++ user_blog/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
 def user_login(request):
-    user_name= request.POST.get('email')
-    password = request.POST.get('password')
-    if user_name and password:
-        user = authenticate(request,username=user_name,password=password)
-        print(user)
-        if user is not None:
-            login(request,user)
-            print('login oldun')
-        else:
-            print('login olamadin')
+    if request.method == "POST":
+        user_name= request.POST.get('email')
+        password = request.POST.get('password')
+        if user_name and password:
+            user = authenticate(request,username=user_name,password=password)
+            print(user)
+            if user is not None:
+                login(request,user)
+                print('login oldun')
+            else:
+                print('login olamadin')
     return render(request,'user_blog/login.html',{})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
